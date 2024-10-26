458630dacbf3d9a1712c1bb32a3d423469bffd60
Warish <warishess.m@gmail.com>
2024-10-26 16:53:50 -0700

    Updated astro.config.mjs
    Sidebar, TOC, style.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,17 +23,21 @@ export default defineConfig({
 					autogenerate: { directory: 'getting-started' },
 				},
 				{
-					label: 'Guides',
+					label: 'Cosmic Navigation',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ slug: 'cosmic-navigation/reading-star-charts-and-maps' },
+						{ label: 'Plotting a Course Through Space', slug: 'cosmic-navigation/plotting-course-through-space' },
+						'cosmic-navigation/dealing-with-gravity-and-orbit', // shorthand for an internal link
 					],
 				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				{ label: 'Example page', link: '/example/' },
+				{ label: 'Visit my portfolio', link: 'https://warish.co/' }, // An external link to the NASA website.
 			],
+			customCss: [
+				// Path to your custom CSS file
+				'./src/styles/style.css',
+		  ],
 		}),
 	],
 });
---
+++ src/content/docs/cosmic-navigation/plotting-course-through-space.md
@@ -0,0 +1,11 @@
+---
+title: Plotting a Course Through Space
+description: Learn how to plot a course through space.
+---
+
+Guides lead a user through a specific task they want to accomplish, often with a sequence of steps.
+Writing a good guide requires thinking about what your users are trying to do.
+
+## Further reading
+
+- Read [about how-to guides](https://diataxis.fr/how-to-guides/) in the Diátaxis framework
